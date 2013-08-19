3de87035d7af28e6e2b7b8537d2bb5298c9bde67
jcpeden <jcpeden@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-19 09:04:50 +0000

    Modified JS
    git-svn-id: https://plugins.svn.wordpress.org/easy-wp-cookie-popup/trunk@758314 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/popup.js
+++ js/popup.js
@@ -7,7 +7,7 @@
 
 (function($){
 if (document.cookie.indexOf('visited=true') == -1) {
-	var fifteenDays = 1000*60*60*24*15;
+	var fifteenDays = 60*60*24*15;
 	var expires = new Date((new Date()).valueOf() + fifteenDays);
 	document.cookie = "visited=true;expires=" + expires.toUTCString();
 	$('#eu-cookie').show("fast");
