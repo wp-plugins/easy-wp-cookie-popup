4a3e3831c1fdadd0b7aa4cadd30c95edccf87166
jcpeden <jcpeden@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-11 22:39:11 +0000

    Tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/easy-wp-cookie-popup/tags/1.0.2@802786 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/constants.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Constants used by this plugin
+ * 
+ * @package WPCOOKIES
+ * 
+ * @author jcpeden
+ * @version 1.0.2
+ * @since 1.0.0
+ */
+
+// The current version of this plugin
+if( !defined( 'WPCOOKIES_VERSION' ) ) define( 'WPCOOKIES_VERSION', '1.0.2' );
+
+// The directory the plugin resides in
+if( !defined( 'WPCOOKIES_DIRNAME' ) ) define( 'WPCOOKIES_DIRNAME', dirname( dirname( __FILE__ ) ) );
+
+// The URL path of this plugin
+if( !defined( 'WPCOOKIES_URLPATH' ) ) define( 'WPCOOKIES_URLPATH', WP_PLUGIN_URL . "/" . plugin_basename( WPCOOKIES_DIRNAME ) );
+
+if( !defined( 'IS_AJAX_REQUEST' ) ) define( 'IS_AJAX_REQUEST', ( !empty( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && strtolower( $_SERVER['HTTP_X_REQUESTED_WITH'] ) == 'xmlhttprequest' ) );
