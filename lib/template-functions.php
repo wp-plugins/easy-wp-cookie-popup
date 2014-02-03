bee2e6b07138592624d6cdaa44dfc1ac979d6211
jcpeden <jcpeden@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-03 14:49:30 +0000

    Version 1.0.7
    git-svn-id: https://plugins.svn.wordpress.org/easy-wp-cookie-popup/tags/1.0.7@850593 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/template-functions.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Template functions for this plugin
+ * 
+ * Place all functions that may be usable in theme template files here.
+ * 
+ * @package WordpressCookies
+ * 
+ * @author jcpeden
+ * @version 1.0.7
+ * @since 1.0.0
+ */
+
+function cookie_popup() {
+	if (!is_admin()) {
+		global $WPCookies;
+
+		/* Get position of popup */
+		if($WPCookies->get_option( 'position' ) == 'top') {
+			$position = 'top';
+		} else {
+			$position = 'bottom';
+		} 
+
+		/* If WPML is inactive, load default popup */
+		if ( !function_exists ('icl_get_languages') ) { ?>
+			<div id="eu-cookie" class="<?php echo $position; ?>">
+				<div class="close-icon">
+					<a href="javascript:void(0)" id="closecookie"><span>close cookie popup<span></a>
+				</div>
+				<h3><?php echo ($WPCookies->get_option( 'title' )); ?></h3>
+				<p><?php echo ($WPCookies->get_option( 'message' )); ?></p>
+			</div>
+		<?php } else {
+
+			/* If WPML is active, load language specific popup */
+			$languages = icl_get_languages('skip_missing=0&orderby=code');
+			if( !empty( $languages ) ) {
+				foreach ($languages as $l) {
+					if($l['active']) {
+						$id = $l['language_code']; 
+
+						/* Display popup if enabled for language */
+						if ($WPCookies->get_option( 'disable_lang_' .$id ) == 1) { ?>
+							<div id="eu-cookie" class="<?php echo $position; ?>">
+								<div class="close-icon">
+									<a href="javascript:void(0)" id="closecookie"><span>close cookie popup<span></a>
+								</div>
+								<h3><?php echo ($WPCookies->get_option( 'title_lang_' .$id )); ?></h3>
+								<p><?php echo ($WPCookies->get_option( 'message_lang_' .$id )); ?></p>
+							</div>
+						<?php }
+					}
+				}
+	        }
+	    } 
+	}
+}
+add_action('wp_footer', 'cookie_popup');
